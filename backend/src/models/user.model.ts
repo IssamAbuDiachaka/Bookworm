ea6d7811f4c29c625791c7922ffe5e9908d485dc
Issam Abu <issamabudiachaka@gmail.com>
2025-08-05 17:02:03 +0100

    Auth validation and user model done

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,40 @@
+import { Schema, model, Document } from "mongoose";
+
+// User roles
+export enum UserRole {
+  STUDENT = "student",
+  LECTURER = "lecturer",
+  ADMIN = "admin"
+}
+
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: UserRole;
+  program: string;
+  avatar?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: Object.values(UserRole),
+      default: UserRole.STUDENT,
+    },
+    program: { type: String, required: true },
+    avatar: { type: String, default: "" }
+  },
+  {
+    timestamps: true 
+  }
+);
+
+export const User = model<IUser>("User", userSchema);
