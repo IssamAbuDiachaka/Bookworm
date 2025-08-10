454ad2a5bf9c576345c10f2c92d273a1a23c3c47
Issam Abu <issamabudiachaka@gmail.com>
2025-08-10 10:01:27 +0100

    Get user profile controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,8 +1,31 @@
 import { Request, Response } from 'express';
 import {User} from '../models/user.model';
 import { uploadToCloudinary } from '../utils/uploadToCloudinary';
+import { AuthenticatedRequest } from "../middlewares/auth.middleware";
 
-export const uploadImage = async (req: Request, res: Response) => {
+
+export const getUserProfile = async (req: AuthenticatedRequest, res: Response) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.json(user);
+  } catch (err) {
+    console.error("Get profile error:", err);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+
+
+
+export const uploadHandler = async (req: Request, res: Response) => {
   try {
     const file = req.file;
     const userId = req.user?.id;
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,23 +1,21 @@
 import { Router } from "express";
-import  authenticateJWT  from "../middlewares/auth.middleware";
-import { authorizeRoles } from "../middlewares/role.middleware";
+import authenticateJWT from "src/middlewares/auth.middleware";
+import { authorizeRoles } from "src/middlewares/role.middleware";
+import { getUserProfile, uploadHandler } from "@controllers/user.controller";
 
 const router = Router();
 
-router.get(
-  "/profile",
-  authenticateJWT, // must be logged in
-  (req, res) => {
-    res.json({ message: "This is your profile", user: req.user });
-  }
-);
+router.get("/profile", authenticateJWT, getUserProfile);
+
+router.post("/upload", authenticateJWT, authorizeRoles("student", "lecturer"), uploadHandler);
+
 
 router.get(
   "/admin-dashboard",
   authenticateJWT,
   authorizeRoles("admin"), // must be admin
   (req, res) => {
-    res.json({ message: "Welcome, admin!" });
+    res.json({ message: "Admin access granted — list all users here" });
   }
 );
 
